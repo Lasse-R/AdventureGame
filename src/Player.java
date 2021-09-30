5d07b38932ca3a7d44bf35e60e881299ae73c2ae
Baeist <lars18n2@stud.kea.dk>
2021-09-30 16:17:28 +0200

    Minor updates to text outputs Suggested player attributes

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,7 @@
 public class Player {
-    int moveSpeed = 1; //one room at a time
+    private String[] backpack = new String[2];              // maks 3 ting i den(?), mad/eat, gems/bribeMonster, skal bruge en item class
+    private String weapon;                          // weapon class med damage, name, equip, skal bruge enemy class
+    private int hitPoints;                      // afhængig af level feks +10-20 hp per level
+    private String armor;                      // armor class med absorb, name, equip
+    private int level;                              // stiger level efter hvert kill måske, kun 9 rum så nok ikke smart med mere komplekst system
 }
